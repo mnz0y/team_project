9e10f1bbedb0b9439177dcd9e31df6a1f01cab17
mnz0y <156628230+mnz0y@users.noreply.github.com>
2024-02-25 22:23:16 +0900

    20240225

--- js/index.js
+++ js/index.js
@@ -10,3 +10,32 @@ $(function(){
         $('header').removeClass('on');
     });
 });
+
+// 슬라이드 이미지
+    function prev(){
+        $('.slide li:last').prependTo('.slide');
+        $('.slide').css('margin-left',-1920);
+        $('.slide').stop().animate({marginLeft:0},800);
+    }
+    function next(){
+        $('.slide').stop().animate({marginLeft:-1920},800,function(){
+            $('.slide li:first').appendTo('.slide');
+            $('.slide').css({marginLeft:0});
+        });
+    }
+    setInterval(next, 8000);
+$(function(){
+    $('#slideWrap').mouseover(function(){
+        $('#slideWrap span').css('display','block');
+    });
+    $('#slideWrap').mouseout(function(){
+        $('#slideWrap span').css('display','none');
+    });
+
+    $('.prev').click(function(){
+        prev();
+    });
+    $('.next').click(function(){
+        next();
+    });
+});
